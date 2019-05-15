67fd88a472b6911b6fe0f8a2686727be6f4efade
Andre Petrucci <andre_avila_petrucci@hotmail.com>
2019-05-15 03:21:21 -0300

    pokedex home done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './modules/home/home.component';
+
 const routes: Routes = [
-  { path: '', redirectTo: 'pokemon', pathMatch: 'full' },
-  { path: 'pokemon', loadChildren: './modules/pokemon/pokemon.module#PokemonModule' }
+  { path: '', component: HomeComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './modules/home/home.component'
+
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent
   ],
   imports: [
     AppRoutingModule,
